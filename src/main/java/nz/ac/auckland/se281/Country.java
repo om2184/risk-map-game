fffb35534bf06c81e28db34f52ea5bd874415052
Om Patel <opat597@aucklanduni.ac.nz>
2024-05-26 00:22:47 +1200

    created getter methods in Country class and instance variables

---
+++ src/main/java/nz/ac/auckland/se281/Country.java
@@ -0,0 +1,39 @@
+package nz.ac.auckland.se281;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Country {
+
+  private String name;
+  private String continent;
+  private int tax;
+  private List<String> neighbours;
+
+  public Country(String name, String continent, int tax) {
+    this.name = name;
+    this.continent = continent;
+    this.tax = tax;
+    this.neighbours = new ArrayList<>();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getContinent() {
+    return continent;
+  }
+
+  public int getTax() {
+    return tax;
+  }
+
+  public List<String> getNeighbours() {
+    return neighbours;
+  }
+
+  public void addNeighbour(String neighbour) {
+    neighbours.add(neighbour);
+  }
+}
