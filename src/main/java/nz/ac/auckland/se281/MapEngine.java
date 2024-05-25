b876f2fd340e07e91970f67e0155dbd42e3d01b9
Om Patel <opat597@aucklanduni.ac.nz>
2024-05-26 00:27:03 +1200

    added logic to utilise countries list

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -7,7 +7,7 @@ import java.util.Map;
 /** This class is the main entry point. */
 public class MapEngine {
 
-  private Map<String, List<String>> mapData;
+  private Map<String, Country> mapData;
 
   public MapEngine() {
     this.mapData = new HashMap<>();
@@ -19,6 +19,16 @@ public class MapEngine {
     List<String> countries = Utils.readCountries();
     List<String> adjacencies = Utils.readAdjacencies();
     // add code here to create your data structures
+
+    // add code here to populate the mapData
+    for (String country : countries) {
+      String[] countryData = country.split(",");
+      String name = countryData[0];
+      String continent = countryData[1];
+      int tax = Integer.parseInt(countryData[2]);
+      Country newCountry = new Country(name, continent, tax);
+      mapData.put(name, newCountry);
+    }
   }
 
   /** this method is invoked when the user run the command info-country. */
